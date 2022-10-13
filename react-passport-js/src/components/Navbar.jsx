159ccd6b1cb1f437e42cfeed5c4c8e423439ca1b
Achyuth Varma <achyuth.varma1@gmail.com>
2022-10-13 17:58:51 -0400

    Added client

---
+++ react-passport-js/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../";
+
+const Navbar = ({ user }) => {
+  return (
+    <div className="navbar">
+      <span className="logo">
+        <Link className="link" to="/">
+          Achyuth App
+        </Link>
+      </span>
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img src="" alt="" className="avatar" />
+          </li>
+          <li className="listItem">John Doe</li>
+          <li className="listItem">Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
